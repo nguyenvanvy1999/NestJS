8e9127c14f233a002d32fad2e40c3618096b3ed4
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-04-17 17:52:06 +0700

    add : add user tool (parse JSON and delete values)

---
+++ src/user/tools/user.tool.ts
@@ -0,0 +1,19 @@
+import { UserResponse } from '../interfaces/user.response.dto';
+import { User, UserDocument } from '../user.schema';
+
+export class UserTool {
+	removeForOne(user: User): UserResponse {
+		const _ = user.toJSON();
+		const { isActive, password, createdAt, updatedAt, deletedAt, ...result } = _;
+		return result;
+	}
+	removeForMany(users: User[]): UserResponse[] {
+		const results = [];
+		users.forEach((user) => {
+			const _ = user.toJSON();
+			const { isActive, password, createdAt, updatedAt, deletedAt, ...test } = _;
+			results.push(test);
+		});
+		return results;
+	}
+}
