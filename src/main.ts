15e66b8870fe40217b1f7d45a5e39fc42f8f044b
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-04-24 22:11:30 +0700

    edit : class start app

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
-import express from 'express';
-import path from 'path';
-import favicon from 'serve-favicon';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import morgan from 'morgan';
-import { AppLogger, stream } from './tools/logger.tool';
+import { AppLogger } from './tools/logger.tool';
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { logger: AppLogger });
-	app.enableCors();
-	app.use(morgan('dev'));
-	app.use(morgan(':method :url :status :res[content-length] - :response-time ms', { stream: stream }));
-	app.use(helmet());
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
-	app.use(favicon(path.join(__dirname, '../public/favicon.ico')));
-	const config = new DocumentBuilder()
-		.setTitle('Mesthing API')
-		.setDescription('IOT API ver 1')
-		.setVersion('1.0')
-		.addTag('user')
-		.build();
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, document);
-	await app.listen(3000);
-}
-bootstrap();
+import { StartApp } from './setup';
+const app = new StartApp(NestFactory.create(AppModule, { logger: AppLogger }));
+app.startServer();
---
+++ src/setup.ts
@@ -0,0 +1,43 @@
+import helmet from 'helmet';
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import morgan from 'morgan';
+import { stream } from './tools/logger.tool';
+import { INestApplication } from '@nestjs/common';
+
+export class StartApp {
+	private readonly app: Promise<INestApplication>;
+	constructor(app: Promise<INestApplication>) {
+		this.app = app;
+		this.initializeMiddlewares();
+		this.initializeSwagger();
+	}
+
+	private async initializeMiddlewares() {
+		const server = await this.app;
+		server.enableCors();
+		server.use(morgan(':method :url :status :res[content-length] - :response-time ms', { stream: stream }));
+		server.use(helmet());
+		server.use(express.json());
+		server.use(express.urlencoded({ extended: true }));
+		server.use(favicon(path.join(__dirname, '../public/favicon.ico')));
+	}
+	private async initializeSwagger() {
+		const server = await this.app;
+		const config = new DocumentBuilder()
+			.setTitle('Mesthing API')
+			.setDescription('IOT API ver 1')
+			.setVersion('1.0')
+			.addTag('user')
+			.build();
+		const document = SwaggerModule.createDocument(server, config);
+		SwaggerModule.setup('apis', server, document);
+	}
+
+	public async startServer() {
+		const server = await this.app;
+		await server.listen(3000);
+	}
+}
